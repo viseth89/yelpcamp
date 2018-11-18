ccf53a33af80f62a470d74c35e569c5067536527
Viseth Sen <viseth89@gmail.com>
2018-11-18 08:20:38 -0600

    post to list/campgrounds

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 var express = require('express')
 // var request = require('request')
 var app = express();
-app.set('view engine', 'ejs')
+var bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', function(req, res){
   res.render('landing');
 });
 
-app.get('/campgrounds', function(req, res){
-  var campgrounds = [
-    {name:'Solo Site', image: 'https://survivallife.com/wp-content/uploads/2017/06/campgrounds-feature.jpg'},
-    {name:'Yaha Clan', image: 'https://survivallife.com/wp-content/uploads/2017/06/campgrounds-feature.jpg'},
-    {name:'Indian Trail', image: 'https://survivallife.com/wp-content/uploads/2017/06/campgrounds-feature.jpg'},
-    {name:'Indian Trail', image: 'https://survivallife.com/wp-content/uploads/2017/06/campgrounds-feature.jpg'},
-    {name:'Indian Trail', image: 'https://survivallife.com/wp-content/uploads/2017/06/campgrounds-feature.jpg'},
-    {name:'Indian Trail', image: 'https://survivallife.com/wp-content/uploads/2017/06/campgrounds-feature.jpg'}
+var campgrounds = [
+  {name:'Solo Site', image: 'https://survivallife.com/wp-content/uploads/2017/06/campgrounds-feature.jpg'},
+  {name:'Yaha Clan', image: 'https://survivallife.com/wp-content/uploads/2017/06/campgrounds-feature.jpg'},
+  {name:'Indian Trail', image: 'https://survivallife.com/wp-content/uploads/2017/06/campgrounds-feature.jpg'},
+  {name:'Indian Trail', image: 'https://survivallife.com/wp-content/uploads/2017/06/campgrounds-feature.jpg'},
+  {name:'Indian Trail', image: 'https://survivallife.com/wp-content/uploads/2017/06/campgrounds-feature.jpg'},
+  {name:'Indian Trail', image: 'https://survivallife.com/wp-content/uploads/2017/06/campgrounds-feature.jpg'}
 
-  ]
+]
+
+app.get('/campgrounds', function(req, res){
 
   res.render('campgrounds', {campgrounds:campgrounds})
 });
+
+app.post('/campgrounds', function(req, res){
+  var name=req.body.name;
+  var image = req.body.image
+  var newCampground = {name: name, image: image}
+  campgrounds.push(newCampground)
+  //get data from form and add to campgrounds array
+  res.redirect('/campgrounds')
+  //redirect back to campgrounds page
+
+})
+
+app.get('/campgrounds/new', function(req, res){
+  res.render('new.ejs')
+})
 
 
 app.listen(3000,function(){
