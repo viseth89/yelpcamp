1228ee87ef908aa743bded749f654d6077cb8e28
Viseth Sen <viseth89@gmail.com>
2018-12-12 08:21:50 -0600

    working on part 3

--- Authentication/app.js
+++ Authentication/app.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/auth_demo_app');
+var express                 = require('express'),
+    mongoose                = require('mongoose'),
+    passport                = require('passport'),
+    bodyParser              = require('body-parser'),
+    User                    = require('./models/user'),
+    LocalStrategy           = require('passport-local'),
+    passpoartLocalMongoose  = require('passport-local-mongoose');
 
+mongoose.connect('mongodb://localhost/auth_demo_app', { useNewUrlParser: true });
 var app = express();
 app.set('view engine', 'ejs');
+
+app.use(require('express-session')({
+    secret: 'Tyson is the best',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function(req,res){
     res.render('home');
