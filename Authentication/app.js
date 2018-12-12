36d078a8db922571a4c27fb840bb6a7357bf18ed
Viseth Sen <viseth89@gmail.com>
2018-12-12 10:53:26 -0600

    finished video 3 on auth

--- Authentication/app.js
+++ Authentication/app.js
@@ -9,7 +9,7 @@ var express                 = require('express'),
 mongoose.connect('mongodb://localhost/auth_demo_app', { useNewUrlParser: true });
 var app = express();
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('express-session')({
     secret: 'Tyson is the best',
     resave: false,
@@ -22,12 +22,38 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+
+//=========
+//Routes
+//=========
+
 app.get('/', function(req,res){
     res.render('home');
 });
 
 app.get('/secret', function(req, res){
     res.render('secret');
+});
+
+//Auth Routes
+
+//show sign up form
+app.get('/register', function(req, res){
+    res.render('register');
+});
+//handling user sign up
+app.post('/register', function(req, res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register')
+        }
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('/secret');
+        })
+    })
 });
 
 app.listen(3000,function(){
