49858e947e607ad26ebd7088ff395ecf0a28cfe6
Viseth Sen <viseth89@gmail.com>
2018-12-12 21:42:52 -0600

    Finished auth, beginning refactoring

--- v6/app.js
+++ v6/app.js
@@ -29,6 +29,12 @@ passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//Understand better 12/12
+app.use(function(req,res, next){
+  res.locals.currentUser = req.user;
+  next();
+})
+
 app.get('/', function (req, res) {
     res.render('landing');
 });
@@ -40,7 +46,7 @@ app.get('/campgrounds', function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render('campgrounds/index', {campgrounds: allCampgrounds});
+            res.render('campgrounds/index', {campgrounds: allCampgrounds, currentUser: req.user});
         }
         //res.render('campgrounds', {campgrounds:campgrounds})
     });
@@ -87,7 +93,7 @@ app.get('/campgrounds/:id', function(req, res){
 // COMMENTS ROUTES
 //============
 
-app.get("/campgrounds/:id/comments/new", function(req,res){
+app.get("/campgrounds/:id/comments/new",isLoggedIn, function(req,res){
   //find campground by id
 
   Campground.findById(req.params.id, function(err,campground){
@@ -99,7 +105,7 @@ app.get("/campgrounds/:id/comments/new", function(req,res){
   })
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments",isLoggedIn, function(req, res){
    //lookup campground using ID
    Campground.findById(req.params.id, function(err, campground){
        if(err){
@@ -144,7 +150,31 @@ app.post('/register', function(req, res){
   });
 });
 
+//show login form
+app.get('/login', function(req, res){
+  res.render('login');
+});
 
+//handling login logic
+app.post('/login', passport.authenticate('local',
+  {
+    successRedirect: '/campgrounds',
+    failureRedirect: 'login'
+  }), function(req, res){
+});
+
+//logout ROUTES
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/campgrounds')
+})
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/login');
+}
 app.listen(3000, function () {
     console.log('YelpCamp is serving on port 3000')
 });
