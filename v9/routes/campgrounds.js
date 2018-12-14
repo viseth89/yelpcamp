922792c8ed3ade372f07505987cc2470de4337b5
Viseth Sen <visethsen@Viseths-MacBook-Pro.local>
2018-12-14 08:58:30 -0600

    finished d up v9, working on new features section

---
+++ v9/routes/campgrounds.js
@@ -0,0 +1,66 @@
+var express=require('express');
+var router=express.Router();
+var Campground = require('../models/campground')
+
+//INDEX-SHOW all campgrounds
+router.get('/', function (req, res) {
+    //get all campgrounds from db
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('campgrounds/index', {campgrounds: allCampgrounds, currentUser: req.user});
+        }
+    });
+});
+
+//Create - add new campground to db
+router.post('/', isLoggedIn, function (req, res) {
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var author = {
+      id: req.user._id,
+      username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description: desc, author: author};
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect('/campgrounds')
+            console.log(newlyCreated);
+        }
+    });
+});
+
+//new - show form to create new campground
+router.get('/new', isLoggedIn, function (req, res) {
+    res.render('campgrounds/new')
+});
+
+//show - shows more info
+router.get('/:id', function(req, res){
+    //find campground with provided id
+    Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+          console.log(foundCampground)
+            // render show template with that campground
+            res.render('campgrounds/show', {campground: foundCampground});
+        }
+    });
+});
+
+//middleware
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  };
+  res.redirect('/login');
+};
+
+module.exports = router
