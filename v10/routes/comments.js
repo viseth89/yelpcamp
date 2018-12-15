8df8ac658ca0e4458693e7f68752f9e3527ec6e0
viseth89 <viseth89@gmail.com>
2018-12-14 19:22:26 -0600

    finished edit and update working on destroy

--- v10/routes/comments.js
+++ v10/routes/comments.js
@@ -46,6 +46,28 @@ router.post("/", isLoggedIn, function(req, res) {
   });
 });
 
+//Comments Edit Route
+router.get('/:comment_id/edit', function(req, res){
+  Comment.findById(req.params.comment_id, function(err, foundComment){
+    if(err){
+      res.redirect('back');
+    } else {
+      res.render('comments/edit', {campground_id: req.params.id, comment: foundComment})
+    }
+  })
+})
+
+//Comments Update
+router.put('/:comment_id', function(req, res){
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+    if(err){
+      res.redirect('back');
+    } else {
+      res.redirect('/campgrounds/' + req.params.id);
+    }
+  })
+})
+
 //middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
