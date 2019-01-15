2916ae09314921eb93686163aa26c0005a578c56
viseth89 <viseth89@gmail.com>
2019-01-15 09:58:55 -0600

    added settings for heroku at bottom commented out

--- take2/v2/app.js
+++ take2/v2/app.js
@@ -32,6 +32,7 @@ app.get('/', function(req, res){
     res.render('landing')
 });
 
+// Index - Show all Campgrounds
 app.get('/campgrounds', function(req, res){
     // Get all campgrounds from DB
     Campground.find({}, function(err, allCampgrounds){
@@ -43,19 +44,35 @@ app.get('/campgrounds', function(req, res){
     })
 });
 
+// Create Route - Add New campground to DB -----> 2 of 2
 app.post("/campgrounds", function(req, res){
     //get data from form and add to campgrounds array
     var name = req.body.name
     var image = req.body.image
     var newCampground = {name:name, image: image}
-    campgrounds.push(newCampground)
-    // redirect back to campgrounds page
-    res.redirect("/campgrounds") 
-})
+    // create a new campground and save to db
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect('/campgrounds');
+        }
+    });
+});
 
+// New Route - Show form to create new campground ---- > 1 of 2
 app.get("/campgrounds/new", function (req,res){
     res.render('new')
 })
 
+app.get('/campgrounds/:id', function(req, res){
+    res.send('This will be the show page one day!')
+})
+
 app.listen(3000);
 console.log('YelpCamp has started on port 3000')
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+//     console.log("the yelpcamp server has started ");
+// });
