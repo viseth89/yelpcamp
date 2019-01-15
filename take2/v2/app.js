c12ff6a16db8a347b1471203eb3f7844480d70ae
viseth89 <viseth89@gmail.com>
2019-01-14 18:32:58 -0600

    video 298, 11min mark

--- take2/v2/app.js
+++ take2/v2/app.js
@@ -1,30 +1,46 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser')
+var express             = require('express'),
+    app                 = express(),
+    bodyParser          = require('body-parser'),
+    mongoose            = require('mongoose');
 
-
+mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
 
-var campgrounds = [
-    {name: 'volkswagen', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f7c570a0eeb3bd_340.jpg'},
-    {name: 'salmon creek', image: 'https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f7c570a0eeb3bd_340.jpg'},
-    {name: 'salmon creek', image: 'https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f7c570a0eeb3bd_340.jpg'},
-    {name: 'volkswagen', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f7c570a0eeb3bd_340.jpg'},
-    {name: 'salmon creek', image: 'https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f7c570a0eeb3bd_340.jpg'},
-    {name: 'salmon creek', image: 'https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f7c570a0eeb3bd_340.jpg'},
-    {name: 'volkswagen', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f7c570a0eeb3bd_340.jpg'},
-    {name: 'salmon creek', image: 'https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f7c570a0eeb3bd_340.jpg'},
-    {name: 'salmon creek', image: 'https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f7c570a0eeb3bd_340.jpg'},
-]
+//SCHEMA SETUP
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+})
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+//     {   
+//         name: 'volkswagen', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f7c570a0eeb3bd_340.jpg'
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log('newly created campground');
+//             console.log(campground)
+//         }
+//     });
 
 app.get('/', function(req, res){
     res.render('landing')
 });
 
 app.get('/campgrounds', function(req, res){
-    //Name:data passing in
-    res.render('campgrounds', {campgrounds:campgrounds});
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('campgrounds', {campgrounds:allCampgrounds});
+        }
+    })
 });
 
 app.post("/campgrounds", function(req, res){
